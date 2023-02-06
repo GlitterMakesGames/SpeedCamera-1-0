09daa1a8f9031a55f236e6150a505034364e7347
GlitterMakesGames <123193351+GlitterMakesGames@users.noreply.github.com>
2023-02-05 19:36:51 -0600

    Driver Class & Car (Full Solution)
    The SpeedCamera Class, the Car class, and all the little Visual Basic shenanigans in between. The output should be exactly what I posted on Discord (assuming you type in the same values). If this is not the case, please contact me! Thanks!

---
+++ SpeedCamera/SpeedCamera/Car.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * Author: Delaina Hardwick
+ * Date: 2/4/2023
+ * 
+ * This is the class for "Car" objects
+ */
+
+class Car
+{
+    //Backing Fields
+    private string _make;
+    private string _model;
+    private int _speed;
+    private int _demerits;
+
+    //Main Fields
+    public string make { get => this._make; set => this._make = value; }
+    public string model { get => this._model; set => this._model = value; }
+    public int speed { get => this._speed; set => this._speed = value; }
+    public int demerits { get => this._demerits; set => this._demerits = value; }
+
+    public Car(string a, string b, int c)
+    {
+        make = a;
+        model = b;
+        speed = c;
+    }
+
+    public void calculateDemerits(int speedLimit)
+    {
+        demerits = 0;
+        if (speed <= speedLimit)
+        {
+            demerits = 0;
+        }
+        else
+        {
+            int tempSpeed = speed - (speed % 5);
+            do
+            {
+                demerits++;
+                tempSpeed -= 5;
+            }
+            while (tempSpeed > speedLimit);
+        }
+    }
+
+    public void outputCarInfo()
+    {
+        Console.Write(make + " " + model + " " + speed + "mph: ");
+        if (demerits == 0)
+        {
+            Console.Write("OK");
+        }
+        else if (demerits == -1)
+        {
+            Console.Write("Too Slow!");
+        }
+        else
+        {
+            Console.Write(demerits + " demerits");
+        }
+
+        if (demerits >= 10)
+        {
+            Console.Write(" <LICENSE SUSPENDED>");
+        }
+        Console.Write("\n");
+    }
+}
---
+++ SpeedCamera/SpeedCamera/SpeedCamera.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * Author: Delaina Hardwick
+ * Date: 2/4/2023
+ * 
+ * This is the class containing main
+ */
+class SpeedCamera
+{
+    public static void Main(String[] args)
+    {
+        int speedLimit;
+
+        Car car1;
+        Car car2;
+        Car car3;
+
+        string tempM1;
+        string tempM2;
+        int tempSp;
+        int tempDem;
+
+        //---------------------------USER INPUT---------------------------//
+
+        Console.Write("Please enter the speed limit: ");
+        speedLimit = Int32.Parse(Console.ReadLine());
+        Console.Write("\n");
+
+        //USER INPUT FOR THE CAR1 OBJECT//
+        Console.Write("Please enter the make of the first car: ");
+        tempM1 = Console.ReadLine();
+        //------------------
+        Console.Write("Please enter the model of the first car: ");
+        tempM2 = Console.ReadLine();
+        //------------------
+        Console.Write("Please enter the speed of the first car: ");
+        tempSp = Int32.Parse(Console.ReadLine());
+        //------------------
+        car1 = new Car(tempM1, tempM2, tempSp);
+
+        //USER INPUT FOR THE CAR2 OBJECT//
+        Console.Write("Please enter the make of the second car: ");
+        tempM1 = Console.ReadLine();
+        //------------------
+        Console.Write("Please enter the model of the second car: ");
+        tempM2 = Console.ReadLine();
+        //------------------
+        Console.Write("Please enter the speed of the second car: ");
+        tempSp = Int32.Parse(Console.ReadLine());
+        //------------------
+        car2 = new Car(tempM1, tempM2, tempSp);
+
+        //USER INPUT FOR THE CAR3 OBJECT//
+        Console.Write("Please enter the make of the third car: ");
+        tempM1 = Console.ReadLine();
+        //------------------
+        Console.Write("Please enter the model of the third car: ");
+        tempM2 = Console.ReadLine();
+        //------------------
+        Console.Write("Please enter the speed of the third car: ");
+        tempSp = Int32.Parse(Console.ReadLine());
+        //------------------
+        car3 = new Car(tempM1, tempM2, tempSp);
+
+        //---------------------------CALCULATIONS & FINAL OUTPUT---------------------------//
+
+        car1.calculateDemerits(speedLimit); //infinite loop?
+        car2.calculateDemerits(speedLimit);
+        car3.calculateDemerits(speedLimit);
+
+        Console.WriteLine();
+        Console.WriteLine("Speed Limit: " + speedLimit + "mph");
+
+        car1.outputCarInfo();
+        car2.outputCarInfo();
+        car3.outputCarInfo();
+    }
+}
